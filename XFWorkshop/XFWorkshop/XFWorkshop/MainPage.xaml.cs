8fb3143d7640580ce07588cb1ee28e45665740a3
Igor Gorelik <igor.gorelik@bigfoot.com>
2017-07-12 11:37:19 +1000

    Xmarin.Forms initial push

---
+++ XFWorkshop/XFWorkshop/XFWorkshop/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFWorkshop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTestClicked(object sender, EventArgs e)
+        {
+            lblTest.Text = "Test is tapped";
+        }
+    }
+}
